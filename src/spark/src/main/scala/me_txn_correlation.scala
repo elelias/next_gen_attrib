ccf1b6749b6991fe15b83a253ef2e8668e16090e
eron <eron@bicharraco-7384.lvs01.dev.ebayc3.com>
2017-11-24 14:28:38 +0100

    changes

--- src/spark/src/main/scala/me_txn_correlation.scala
+++ src/spark/src/main/scala/me_txn_correlation.scala
@@ -2,6 +2,8 @@
 /* SimpleApp.scala */
 import org.apache.spark.sql.Row
 import org.apache.spark.sql.SparkSession
+import org.apache.log4j.{Level, Logger}
+import org.apache.spark.storage.StorageLevel
 
 
 object CorrelationApp {
@@ -16,40 +18,53 @@ object CorrelationApp {
    .getOrCreate()
    val sc    = spark.sparkContext
    sc.setLogLevel("ERROR")
+   Logger.getLogger("org").setLevel(Level.ERROR)
+
+   println("broadcast threshold is "+spark.conf.get("spark.sql.autoBroadcastJoinThreshold").toInt / 1024 / 1024 ) 
+
 
    import spark.implicits._
    import spark.sql
+   import org.apache.spark.sql.SaveMode
    
    args.foreach(println)
+   println("HELLO")
 
    val cko_start_dt   = args(0)
    val cko_end_dt     = args(1)
    val data_source    = args(2)
    val mktng_start_dt = args(3)
    val mktng_end_dt   = cko_end_dt
+   //val mktng_end_dt   = args(4)
    
    
-   val txns_path =  "hdfs://apollo-phx-nn-ha/user/hive/warehouse/txns.db/txns_with_xid_parquet/"
-   val mktng_path = "hdfs://apollo-phx-nn-ha/user/hive/warehouse/mktng.db/marketing_events_parquet/"
+   val txns_path =  s"hdfs://apollo-phx-nn-ha/user/hive/warehouse/txns.db/txn_events_with_bbowa_incdata"
+
+   val mktng_path = s"hdfs://apollo-phx-nn-ha/user/hive/warehouse/mktng.db/marketing_events_parquet/data_source=$data_source"
       
 
-
-
    val txns_df = spark.read.load(txns_path).filter(s"dt between '$cko_start_dt' and '$cko_end_dt'")
+   
+   txns_df.cache()
+   //val txns_count = txns_df.count()
+   //println("there are "+txns_count+" transactions")
+
    txns_df.createOrReplaceTempView("txns")
 
-   val mktng_df  = spark.read.load(mktng_path).filter(s"dt between '$mktng_start_dt' and '$mktng_end_dt' and data_source='$data_source'")
+   val mktng_df  = spark.read.load(mktng_path).filter(s"dt between '$mktng_start_dt' and '$mktng_end_dt'")
+   //mktng_df.persist(StorageLevel.MEMORY_AND_DISK)
    mktng_df.createOrReplaceTempView("mktng")
 
 
+
+   
  
-   val common_join_query = """
-   select
-   distinct
+   val common_join_query = s"""
+   select   /*+ BROADCAST (B) */
    A.guid,
    A.cguid,
    A.user_id,
-   A.event_id,
+   cast(A.event_id as string) as event_id,
    A.event_id_type,
    A.event_type_id,
    A.channel_id,
@@ -83,9 +98,9 @@ object CorrelationApp {
    B.seller_country_id,
    B.buyer_id,
    B.buyer_country_id,
-   B.FM_BUYER_TYPE_CD,
-   B.FM_BUYER_TYPE_DESC,
-   B.IS_FIRST_PURCHASE,
+   B.fm_buyer_type_cd,
+   B.fm_buyer_type_desc,
+   B.is_first_purchase,
    B.created_dt,
    B.created_time,
    B.gmb_usd,
@@ -109,8 +124,8 @@ object CorrelationApp {
    B.bbowa_device_type_level2,
    B.bbowa_traffic_source_id,
    B.sess_device_type,
-   B.sess_experience_level1,
-   B.sess_experience_level2,
+   B.sess_exp_level1 as sess_experience_level1,
+   B.sess_exp_level2 as sess_experience_level2,
    B.sess_device_type_level1,
    B.sess_device_type_level2,
    B.gaid,
@@ -119,11 +134,15 @@ object CorrelationApp {
    B.incdata_id as cko_incdata_id,
    B.incdata_dqscore as cko_incdata_dqscore,
    'uid' as join_strategy,
-   (unix_timestamp(bbowa_event_timestamp) - unix_timestamp(event_ts))/86400. as day_diff,
-   unix_timestamp(bbowa_event_timestamp) - unix_timestamp(event_ts) as sec_diff,
+   (unix_timestamp(bbowa_event_timestamp) - unix_timestamp(event_ts))/86400. as bbowa_day_diff,
+    unix_timestamp(bbowa_event_timestamp) - unix_timestamp(event_ts)         as bbowa_sec_diff,
+   (unix_timestamp(created_time) - unix_timestamp(event_ts))/86400.          as cko_day_diff,
+    unix_timestamp(created_time) - unix_timestamp(event_ts)                  as cko_sec_diff,
+   case when (unix_timestamp(bbowa_event_timestamp) - unix_timestamp(event_ts)) > -600 then 1 else 0 end as is_before_bbowa,
+   case when (unix_timestamp(created_time) - unix_timestamp(event_ts)) > -600 then 1 else 0 end            as is_before_checkout,
+   
    -1 as is_same_device,
-   -1 as is_same_exp,
-   '$data_source' as data_source
+   -1 as is_same_exp
    """
 
 
@@ -142,15 +161,19 @@ object CorrelationApp {
    inner join 	uid_mktng A
    on    A.user_id = B.buyer_id
 
-   where (A.event_dt <= B.bbowa_session_start_dt)
-   and   (datediff(B.bbowa_session_start_dt, A.event_dt) <= 30)
-   and   (unix_timestamp(A.event_ts) <= unix_timestamp(B.bbowa_event_timestamp) + 600)
-   and   (unix_timestamp(B.bbowa_event_timestamp) - unix_timestamp(A.event_ts) <= 2592000)
+   where (A.event_dt <= B.created_dt)
+   and   (datediff(B.created_dt, A.event_dt) <= 14+10+1)
+   and   (unix_timestamp(A.event_ts) <= unix_timestamp(B.created_time) + 600)
+   and   (unix_timestamp(B.created_time) - unix_timestamp(A.event_ts) <= (14+10)*86400)
    """
 
    val uid_join_query = common_join_query + uid_join_from_query
    val uid_join_df = sql(uid_join_query)
    uid_join_df.createOrReplaceTempView("uid_join")
+   
+   //uid_join_df.cache()
+   //val uid_join_count = uid_join_df.count()
+   //println("uid count "+uid_join_count)
    //================================
 
 
@@ -184,10 +207,10 @@ object CorrelationApp {
    FROM        mktng_deviceid      A
    INNER JOIN  txns_deviceid       B
    on          A.device_id = B.device_id
-   where (A.event_dt <= B.bbowa_session_start_dt)
-   and   (datediff(B.bbowa_session_start_dt, A.event_dt) <= 30)
-   and   (unix_timestamp(A.event_ts) <= unix_timestamp(B.bbowa_event_timestamp) + 600)
-   and   (unix_timestamp(B.bbowa_event_timestamp) - unix_timestamp(A.event_ts) <= 2592000)   
+   where (A.event_dt <= B.created_dt)
+   and   (datediff(B.created_dt, A.event_dt) <= 14+10+1)
+   and   (unix_timestamp(A.event_ts) <= unix_timestamp(B.created_time) + 600)
+   and   (unix_timestamp(B.created_time) - unix_timestamp(A.event_ts) <= (14+10)*86400)   
    """
    val deviceid_join_query = common_join_query + deviceid_join_from_query 
    val deviceid_join_df    = sql(deviceid_join_query)
@@ -209,13 +232,18 @@ object CorrelationApp {
    INNER JOIN  txns_cguid B
    on          A.CGUID = B.BBOWA_CGUID
 
-   where (A.event_dt <= B.bbowa_session_start_dt)
-   and  (datediff(B.bbowa_session_start_dt, A.event_dt) <= 30)
-   and   (unix_timestamp(A.event_ts) <= unix_timestamp(B.bbowa_event_timestamp) + 600)
-   and (unix_timestamp(B.bbowa_event_timestamp) - unix_timestamp(A.event_ts) <= 2592000)   
+   where (A.event_dt <= B.created_dt)
+   and  (datediff(B.created_dt, A.event_dt) <= 14+10+1)
+   and  (unix_timestamp(A.event_ts) <= unix_timestamp(B.created_time) + 600)
+   and  (unix_timestamp(B.created_time) - unix_timestamp(A.event_ts) <= (14+10)*86400)
    """
    val cguid_join_query = common_join_query + cguid_join_from_query 
    val cguid_join_df    = sql(cguid_join_query)
+
+   //cguid_join_df.cache()
+   //val cguid_join_count = cguid_join_df.count()
+   //println("cguid_join_count = "+cguid_join_count)
+
    cguid_join_df.createOrReplaceTempView("cguid_join")
    //================================
 
@@ -237,10 +265,10 @@ object CorrelationApp {
    INNER JOIN  txns_xid       B
    on          A.INCDATA_ID = B.INCDATA_ID
 
-   where (A.event_dt <= B.bbowa_session_start_dt)
-   and  (datediff(B.bbowa_session_start_dt, A.event_dt) <= 30)
-   and   (unix_timestamp(A.event_ts) <= unix_timestamp(B.bbowa_event_timestamp) + 600)
-   and (unix_timestamp(B.bbowa_event_timestamp) - unix_timestamp(A.event_ts) <= 2592000)   
+   where (A.event_dt <= B.created_dt)
+   and   (datediff(B.created_dt, A.event_dt) <= 14+10+1)
+   and   (unix_timestamp(A.event_ts) <= unix_timestamp(B.created_time) + 600)
+   and   (unix_timestamp(B.created_time) - unix_timestamp(A.event_ts) <= (14+10) *86400)
    """
    val xid_join_query = common_join_query + xid_join_from_query 
    val xid_join_df    = sql(xid_join_query)
@@ -293,9 +321,9 @@ object CorrelationApp {
    
    coalesce(A.buyer_id, B.buyer_id) as buyer_id,
    coalesce(A.buyer_country_id, B.buyer_country_id) as buyer_country_id,
-   coalesce(A.FM_BUYER_TYPE_CD, B.FM_BUYER_TYPE_CD) as FM_BUYER_TYPE_CD,
-   coalesce(A.FM_BUYER_TYPE_DESC, B.FM_BUYER_TYPE_DESC) as FM_BUYER_TYPE_DESC,
-   coalesce(A.IS_FIRST_PURCHASE, B.IS_FIRST_PURCHASE) as IS_FIRST_PURCHASE,
+   coalesce(A.fm_buyer_type_cd, B.fm_buyer_type_cd) as fm_buyer_type_cd,
+   coalesce(A.fm_buyer_type_desc, B.fm_buyer_type_desc) as fm_buyer_type_desc,
+   coalesce(A.is_first_purchase, B.is_first_purchase) as is_first_purchase,
    
    coalesce(A.created_dt, B.created_dt) as created_dt,
    coalesce(A.created_time, B.created_time) as created_time,
@@ -335,15 +363,17 @@ object CorrelationApp {
    coalesce(A.cko_incdata_dqscore, B.cko_incdata_dqscore) as cko_incdata_dqscore,
 
    coalesce(A.join_strategy, B.join_strategy) as join_strategy,
-   coalesce(A.day_diff, B.day_diff) as day_diff,
-   coalesce(A.sec_diff, B.sec_diff) as sec_diff,
+   coalesce(A.bbowa_day_diff, B.bbowa_day_diff) as bbowa_day_diff,
+   coalesce(A.bbowa_sec_diff, B.bbowa_sec_diff) as bbowa_sec_diff,
+   coalesce(A.cko_day_diff, B.cko_day_diff) as cko_day_diff,
+   coalesce(A.cko_sec_diff, B.cko_sec_diff) as cko_sec_diff,
+
    coalesce(A.is_same_device, B.is_same_device) as is_same_device,
-   coalesce(A.is_same_exp, B.is_same_exp) as is_same_exp,
-   coalesce(A.data_source, B.data_source) as data_source
+   coalesce(A.is_same_exp, B.is_same_exp) as is_same_exp
 
 
 
-   FROM 			uid_join         A
+   FROM 			uid_join           A
    FULL OUTER JOIN		deviceid_join      B
    ON   	   		A.event_id       = B.event_id
    AND			        A.item_id        = B.item_id
@@ -353,9 +383,9 @@ object CorrelationApp {
    val uid_deviceid_join_df = sql(uid_deviceid_join_query)
    uid_deviceid_join_df.createOrReplaceTempView("uid_deviceid_join")
 
-
-
-
+   //uid_deviceid_join_df.cache()
+   //val uid_deviceid_join_count = uid_deviceid_join_df.count()
+   //println("joined uid_device_id = "+uid_deviceid_join_count)
 
 
 
@@ -402,9 +432,9 @@ object CorrelationApp {
    
    coalesce(A.buyer_id, B.buyer_id) as buyer_id,
    coalesce(A.buyer_country_id, B.buyer_country_id) as buyer_country_id,
-   coalesce(A.FM_BUYER_TYPE_CD, B.FM_BUYER_TYPE_CD) as FM_BUYER_TYPE_CD,
-   coalesce(A.FM_BUYER_TYPE_DESC, B.FM_BUYER_TYPE_DESC) as FM_BUYER_TYPE_DESC,
-   coalesce(A.IS_FIRST_PURCHASE, B.IS_FIRST_PURCHASE) as IS_FIRST_PURCHASE,
+   coalesce(A.fm_buyer_type_cd, B.fm_buyer_type_cd) as fm_buyer_type_cd,
+   coalesce(A.fm_buyer_type_desc, B.fm_buyer_type_desc) as fm_buyer_type_desc,
+   coalesce(A.is_first_purchase, B.is_first_purchase) as is_first_purchase,
    
    coalesce(A.created_dt, B.created_dt) as created_dt,
    coalesce(A.created_time, B.created_time) as created_time,
@@ -444,11 +474,13 @@ object CorrelationApp {
    coalesce(A.cko_incdata_dqscore, B.cko_incdata_dqscore) as cko_incdata_dqscore,
 
    coalesce(A.join_strategy, B.join_strategy) as join_strategy,
-   coalesce(A.day_diff, B.day_diff) as day_diff,
-   coalesce(A.sec_diff, B.sec_diff) as sec_diff,
+   coalesce(A.bbowa_day_diff, B.bbowa_day_diff) as bbowa_day_diff,
+   coalesce(A.bbowa_sec_diff, B.bbowa_sec_diff) as bbowa_sec_diff,
+   coalesce(A.cko_day_diff,   B.cko_day_diff) as cko_day_diff,
+   coalesce(A.cko_sec_diff,   B.cko_sec_diff) as cko_sec_diff,
+
    coalesce(A.is_same_device, B.is_same_device) as is_same_device,
-   coalesce(A.is_same_exp, B.is_same_exp) as is_same_exp,
-   coalesce(A.data_source, B.data_source) as data_source
+   coalesce(A.is_same_exp, B.is_same_exp) as is_same_exp
 
 
 
@@ -466,7 +498,7 @@ object CorrelationApp {
 
 
 
-   val merge_all_join_query = """
+   val merge_all_join_query = s"""
    select 
    distinct
    coalesce(A.guid, B.guid) as guid,
@@ -509,9 +541,9 @@ object CorrelationApp {
    
    coalesce(A.buyer_id, B.buyer_id) as buyer_id,
    coalesce(A.buyer_country_id, B.buyer_country_id) as buyer_country_id,
-   coalesce(A.FM_BUYER_TYPE_CD, B.FM_BUYER_TYPE_CD) as FM_BUYER_TYPE_CD,
-   coalesce(A.FM_BUYER_TYPE_DESC, B.FM_BUYER_TYPE_DESC) as FM_BUYER_TYPE_DESC,
-   coalesce(A.IS_FIRST_PURCHASE, B.IS_FIRST_PURCHASE) as IS_FIRST_PURCHASE,
+   coalesce(A.fm_buyer_type_cd, B.fm_buyer_type_cd) as fm_buyer_type_cd,
+   coalesce(A.fm_buyer_type_desc, B.fm_buyer_type_desc) as fm_buyer_type_desc,
+   coalesce(A.is_first_purchase, B.is_first_purchase) as is_first_purchase,
    
    coalesce(A.created_dt, B.created_dt) as created_dt,
    coalesce(A.created_time, B.created_time) as created_time,
@@ -549,14 +581,18 @@ object CorrelationApp {
    coalesce(A.cko_device_id, B.cko_device_id) as cko_device_id,
    coalesce(A.cko_incdata_id, B.cko_incdata_id) as cko_incdata_id,
    coalesce(A.cko_incdata_dqscore, B.cko_incdata_dqscore) as cko_incdata_dqscore,
-
    coalesce(A.join_strategy, B.join_strategy) as join_strategy,
-   coalesce(A.day_diff, B.day_diff) as day_diff,
-   coalesce(A.sec_diff, B.sec_diff) as sec_diff,
+
+   coalesce(A.bbowa_day_diff, B.bbowa_day_diff) as bbowa_day_diff,
+   coalesce(A.bbowa_sec_diff, B.bbowa_sec_diff) as bbowa_sec_diff,
+   coalesce(A.cko_day_diff, B.cko_day_diff) as cko_day_diff,
+   coalesce(A.cko_sec_diff, B.cko_sec_diff) as cko_sec_diff,
+
    coalesce(A.is_same_device, B.is_same_device) as is_same_device,
    coalesce(A.is_same_exp, B.is_same_exp) as is_same_exp,
-   coalesce(A.data_source, B.data_source) as data_source,
-   coalesce(A.created_dt, B.created_dt)   as cko_dt,
+
+   '$data_source'                         as data_source,
+   coalesce(A.created_dt, B.created_dt)   as ck_dt,
    coalesce(A.event_dt, B.event_dt)       as mktng_dt
 
 
@@ -568,19 +604,15 @@ object CorrelationApp {
    """
 
    val merge_all_join_df = sql(merge_all_join_query)
+   //val merge_count = merge_all_join_df.count()
+   //println("merge_count  = " + merge_count)
+
    merge_all_join_df
-   .repartition($"data_source",$"cko_dt",$"mktng_dt")
+   .repartition($"data_source",$"ck_dt",$"mktng_dt")
    .write
-   .partitionBy("data_source","cko_dt","mktng_dt")
+   .partitionBy("data_source","ck_dt","mktng_dt")
+   .mode(SaveMode.Append)
    .save("hdfs://apollo-phx-nn-ha/user/hive/warehouse/mktng.db/me_txns_correlation")
    
-   
-
-
-
-
-
-
-
   }
 }
