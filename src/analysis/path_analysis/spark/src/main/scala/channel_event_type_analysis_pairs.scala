b51c3fe88f5faacd7c168ac7f03bd7601223310f
Stefano Baghino <sbaghino@ebay.com>
2018-01-11 15:29:14 +0100

    skip first step if file is there, count edges' weights

--- src/analysis/path_analysis/spark/src/main/scala/channel_event_type_analysis_pairs.scala
+++ src/analysis/path_analysis/spark/src/main/scala/channel_event_type_analysis_pairs.scala
@@ -1,5 +1,3 @@
-/* channel_event_type_analysis.scala */
-
 import org.apache.spark.sql._
 import org.apache.spark.sql.functions._
 import org.apache.spark.sql.expressions.Window
@@ -15,7 +13,7 @@ object ChannelPairs {
 
     txns_df.createOrReplaceTempView("sample")
 
-	 // Select relevant marketing-related info from the table.
+    // Select relevant marketing-related info from the table.
     val marketing_info = sql(
       """select
     distinct
@@ -29,11 +27,22 @@ object ChannelPairs {
     val window = Window.partitionBy("transaction_id", "item_id").orderBy("event_ts")
     val pairs = marketing_info.withColumn("previous_channel_name", lag("channel_name", 1, null).over(window)).withColumn("rank", rank().over(window))
 
+    val channel_pairs_path = "hdfs://apollo-phx-nn-ha/user/hive/warehouse/attrib.db/channel_pairs"
+
+    // Write the channel pairs (unless they are already there)
     pairs
       .write
       .option("sep", "\t")
-      .mode("overwrite")
-      .csv("hdfs://apollo-phx-nn-ha/user/hive/warehouse/attrib.db/channel_pairs")
+      .mode("ignore")
+      .csv(channel_pairs_path)
+
+    val weighted_edges =
+      spark.read.option("sep", "\t").csv(channel_pairs_path).
+        toDF("transaction_id", "item_id", "event_ts", "channel_name", "previous_channel_name", "step").
+        groupBy("channel_name", "previous_channel_name", "step").
+        count().withColumnRenamed("count", "weight")
+
+    weighted_edges.write.options(Map("sep" -> "\t", "header" -> "true")).mode("ignore")
 
     spark.stop()
   }
