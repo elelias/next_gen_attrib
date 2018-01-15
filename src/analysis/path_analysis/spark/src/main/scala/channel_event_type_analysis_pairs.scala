d3a4365cd9138986529a71ccea92fe2082e9317a
Stefano Baghino <sbaghino@ebay.com>
2018-01-15 13:36:19 +0100

    rewrite the query in scala, filter out nulls, add event_type

--- src/analysis/path_analysis/spark/src/main/scala/channel_event_type_analysis_pairs.scala
+++ src/analysis/path_analysis/spark/src/main/scala/channel_event_type_analysis_pairs.scala
@@ -7,42 +7,42 @@ object ChannelPairs {
   def main(args: Array[String]) {
 
     val spark = SparkSession.builder.appName("channel_pairs").master("yarn").getOrCreate()
-    import spark.sql
+    import spark.implicits._
 
     val txns_df = spark.read.load("hdfs://apollo-phx-nn-ha/user/hive/warehouse/attrib.db/me_txns_correlation_subsample")
 
-    txns_df.createOrReplaceTempView("sample")
-
     // Select relevant marketing-related info from the table.
-    val marketing_info = sql(
-      """select
-    distinct
-    coalesce(transaction_id, -1) as transaction_id,
-    coalesce(item_id, -1) as item_id,
-    event_ts,
-	  coalesce(channel_name, "unassigned") as channel_name
-    from sample
-    where event_type_id <> 4""")
+    val marketing_info = txns_df.select(
+      $"transaction_id",
+      $"item_id",
+      $"event_ts",
+      coalesce($"channel_name", lit("unassigned")).as("channel_name"),
+      when($"event_type_id".isin(1, 5, 7, 8), "click").when($"event_type_id" === 6, "open").otherwise("undefined").as("event_type")
+    ).where($"transaction_id".isNotNull and $"item_id".isNotNull and $"event_type_id" =!= 4)
 
     val window = Window.partitionBy("transaction_id", "item_id").orderBy("event_ts")
-    val pairs = marketing_info.withColumn("previous_channel_name", lag("channel_name", 1, null).over(window)).withColumn("rank", rank().over(window))
+    val pairs =
+      marketing_info.
+        withColumn("previous_channel_name", lag("channel_name", 1, null).over(window)).
+        where($"previous_channel_name".isNotNull).
+        withColumn("rank", rank().over(window))
 
     val channel_pairs_path = "hdfs://apollo-phx-nn-ha/user/hive/warehouse/attrib.db/channel_pairs"
 
     // Write the channel pairs (unless they are already there)
-    pairs
-      .write
-      .option("sep", "\t")
-      .mode("ignore")
-      .csv(channel_pairs_path)
+    pairs.
+      write.
+      option("sep", "\t").
+      mode("overwrite").
+      csv(channel_pairs_path)
 
     val weighted_edges =
       spark.read.option("sep", "\t").csv(channel_pairs_path).
-        toDF("transaction_id", "item_id", "event_ts", "channel_name", "previous_channel_name", "step").
-        groupBy("channel_name", "previous_channel_name", "step").
+        toDF("transaction_id", "item_id", "event_ts", "channel_name", "event_type", "previous_channel_name", "step").
+        groupBy("channel_name", "event_type", "previous_channel_name", "step").
         count().withColumnRenamed("count", "weight")
 
-    weighted_edges.write.options(Map("sep" -> "\t", "header" -> "true")).mode("ignore").csv("hdfs://apollo-phx-nn-ha/user/hive/warehouse/attrib.db/weighted_edges")
+    weighted_edges.write.options(Map("sep" -> "\t", "header" -> "true")).mode("overwrite").csv("hdfs://apollo-phx-nn-ha/user/hive/warehouse/attrib.db/weighted_edges")
 
     spark.stop()
   }
