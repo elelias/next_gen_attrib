16c209d1c79feddf8297ec2d9ee6c8b8601150f8
Stefano Baghino <sbaghino@ebay.com>
2018-01-15 14:21:06 +0100

    reformat for spark-shell, use parquet instead of csv

--- src/analysis/path_analysis/spark/src/main/scala/channel_event_type_analysis_pairs.scala
+++ src/analysis/path_analysis/spark/src/main/scala/channel_event_type_analysis_pairs.scala
@@ -1,6 +1,5 @@
 import org.apache.spark.sql._
 import org.apache.spark.sql.functions._
-import org.apache.spark.sql.expressions.Window
 
 object ChannelPairs {
 
@@ -9,15 +8,19 @@ object ChannelPairs {
     val spark = SparkSession.builder.appName("channel_pairs").master("yarn").getOrCreate()
     import spark.implicits._
 
-    val txns_df = spark.read.load("hdfs://apollo-phx-nn-ha/user/hive/warehouse/attrib.db/me_txns_correlation_subsample")
+    /*
+     * You can copy and paste from here to the remote spark-shell
+     */
 
-    // Select relevant marketing-related info from the table.
-    val marketing_info = txns_df.select(
+    import org.apache.spark.sql.expressions.Window
+    val transactions_sample = spark.read.load("hdfs://apollo-phx-nn-ha/user/hive/warehouse/attrib.db/me_txns_correlation_subsample")
+
+    val marketing_info = transactions_sample.select(
       $"transaction_id",
       $"item_id",
       $"event_ts",
-      coalesce($"channel_name", lit("unassigned")).as("channel_name"),
-      when($"event_type_id".isin(1, 5, 7, 8), "click").when($"event_type_id" === 6, "open").otherwise("undefined").as("event_type")
+      when($"event_type_id".isin(1, 5, 7, 8), "click").when($"event_type_id" === 6, "open").otherwise("undefined").as("event_type"),
+      coalesce($"channel_name", lit("unassigned")).as("channel_name")
     ).where($"transaction_id".isNotNull and $"item_id".isNotNull and $"event_type_id" =!= 4)
 
     val window = Window.partitionBy("transaction_id", "item_id").orderBy("event_ts")
@@ -25,24 +28,25 @@ object ChannelPairs {
       marketing_info.
         withColumn("previous_channel_name", lag("channel_name", 1, null).over(window)).
         where($"previous_channel_name".isNotNull).
-        withColumn("rank", rank().over(window))
+        withColumn("step", rank().over(window))
 
     val channel_pairs_path = "hdfs://apollo-phx-nn-ha/user/hive/warehouse/attrib.db/channel_pairs"
 
-    // Write the channel pairs (unless they are already there)
     pairs.
       write.
-      option("sep", "\t").
       mode("overwrite").
-      csv(channel_pairs_path)
+      parquet(channel_pairs_path)
 
     val weighted_edges =
-      spark.read.option("sep", "\t").csv(channel_pairs_path).
-        toDF("transaction_id", "item_id", "event_ts", "channel_name", "event_type", "previous_channel_name", "step").
+      spark.read.option("sep", "\t").parquet(channel_pairs_path).
         groupBy("channel_name", "event_type", "previous_channel_name", "step").
         count().withColumnRenamed("count", "weight")
 
-    weighted_edges.write.options(Map("sep" -> "\t", "header" -> "true")).mode("overwrite").csv("hdfs://apollo-phx-nn-ha/user/hive/warehouse/attrib.db/weighted_edges")
+    weighted_edges.write.mode("overwrite").parquet("hdfs://apollo-phx-nn-ha/user/hive/warehouse/attrib.db/weighted_edges")
+
+    /*
+     * Stop here copying and pasting to the remote spark-shell to prevent closing your spark session
+     */
 
     spark.stop()
   }
