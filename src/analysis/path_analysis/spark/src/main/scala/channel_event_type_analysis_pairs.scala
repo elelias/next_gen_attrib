4dc1c32aad359a7b0bec4ab1b5a3d18239416ac2
Stefano Baghino <sbaghino@ebay.com>
2018-01-17 15:50:52 +0100

    minor refactoring, include device type

--- src/analysis/path_analysis/spark/src/main/scala/channel_event_type_analysis_pairs.scala
+++ src/analysis/path_analysis/spark/src/main/scala/channel_event_type_analysis_pairs.scala
@@ -12,23 +12,35 @@ object ChannelPairs {
      * You can copy and paste from here to the remote spark-shell
      */
 
+    println(s"process started at ${java.time.Instant.now}")
+
+    spark.sparkContext.setLogLevel("FATAL")
+
     import org.apache.spark.sql.expressions.Window
+    import org.apache.spark.sql.Column
+
     val transactions_sample = spark.read.load("hdfs://apollo-phx-nn-ha/user/hive/warehouse/attrib.db/me_txns_correlation_subsample")
+
+    val valid: Column = $"transaction_id".isNotNull and $"item_id".isNotNull
+    val click: Column = $"event_type_id".isin(1, 5, 7, 8)
+    val open: Column = $"event_type_id" === 6
+    val impression: Column = $"event_type_id" === 4
 
     val marketing_info = transactions_sample.select(
       $"transaction_id",
       $"item_id",
       $"event_ts",
-      when($"event_type_id".isin(1, 5, 7, 8), "click").when($"event_type_id" === 6, "open").otherwise("undefined").as("event_type"),
+      when(click, "click").when(open, "open").otherwise("undefined").as("event_type"),
+      coalesce($"device_type_level1", lit("unassigned")).as("event_device"),
       coalesce($"channel_name", lit("unassigned")).as("channel_name")
-    ).where($"transaction_id".isNotNull and $"item_id".isNotNull and $"event_type_id" =!= 4)
+    ).where(valid and not(impression))
 
     val window = Window.partitionBy("transaction_id", "item_id").orderBy("event_ts")
-    val pairs =
-      marketing_info.
-        withColumn("previous_event_type", lag("event_type", 1, null).over(window)).
-        withColumn("previous_channel_name", lag("channel_name", 1, null).over(window)).
-        withColumn("step", rank().over(window))
+    val pairs = marketing_info.
+      withColumn("previous_event_type", lag("event_type", 1, null).over(window)).
+      withColumn("previous_event_device", lag("event_device", 1, null).over(window)).
+      withColumn("previous_channel_name", lag("channel_name", 1, null).over(window)).
+      withColumn("step", rank().over(window))
 
     val channel_pairs_path = "hdfs://apollo-phx-nn-ha/user/hive/warehouse/attrib.db/channel_pairs"
 
@@ -38,11 +50,13 @@ object ChannelPairs {
       parquet(channel_pairs_path)
 
     val weighted_edges =
-      spark.read.option("sep", "\t").parquet(channel_pairs_path).
-        groupBy("channel_name", "event_type", "previous_channel_name", "previous_event_type", "step").
+      spark.read.parquet(channel_pairs_path).
+        groupBy("channel_name", "event_device", "event_type", "previous_channel_name", "previous_event_device", "previous_event_type", "step").
         count().withColumnRenamed("count", "weight")
 
     weighted_edges.write.mode("overwrite").parquet("hdfs://apollo-phx-nn-ha/user/hive/warehouse/attrib.db/weighted_edges")
+
+    println(s"process ended at ${java.time.Instant.now}")
 
     /*
      * Stop here copying and pasting to the remote spark-shell to prevent closing your spark session
